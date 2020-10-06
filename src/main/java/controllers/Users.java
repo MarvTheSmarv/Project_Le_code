9019d59c64a34b5a37879e88111746c7902c60ac
92886 <92886@farnborough.ac.uk>
2020-10-06 10:24:37 +0100

    Yo yoyoyoyoyoyoyo yo

--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -1,13 +1,11 @@
 package controllers;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,7 +16,7 @@ import java.sql.ResultSet;
 
 public class Users{
     @GET
-    @Path("list")
+    @Path("list") //Read method to read from the database
     public String UsersList() {
         System.out.println("Invoked Users.UsersList()");
         JSONArray response = new JSONArray();
@@ -35,6 +33,76 @@ public class Users{
         } catch (Exception exception) {
             System.out.println("Database error: " + exception.getMessage());
             return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+    @GET
+    @Path("get/{UserID}") //Get one record from the database
+    public String GetUser(@PathParam("UserID") Integer UserID) {
+        System.out.println("Invoked Users.GetUser() with UserID " + UserID);
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT name FROM Users WHERE userId = ?");
+            ps.setInt(1, UserID);
+            ResultSet results = ps.executeQuery();
+            JSONObject response = new JSONObject();
+            if (results.next()== true) {
+                response.put("UserID", UserID);
+                response.put("UserName", results.getString(1));
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to get item, please see server console for more info.\"}";
+        }
+    }
+
+    @POST
+    @Path("add") //Create method to create a new record in the database
+    public String UsersAdd(@FormDataParam("UserID") Integer UserID, @FormDataParam("UserName") String UserName) {
+        System.out.println("Invoked Users.UsersAdd()");
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (userId, name) VALUES (?, ?)");
+            ps.setInt(1, UserID);
+            ps.setString(2, UserName);
+            ps.execute();
+            return "{\"OK\": \"Added user.\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+
+    }
+    @POST
+    @Path("update") //Update method to update an existing record in the database
+    public String updateFood(@FormDataParam("UserID") Integer UserID, @FormDataParam("UserName") String UserName) {
+        try {
+            System.out.println("Invoked Users.UpdateUsers/update UserID=" + UserID);
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE Users SET name = ? WHERE userId = ?");
+            ps.setString(1, UserName);
+            ps.setInt(2, UserID);
+            ps.execute();
+            return "{\"OK\": \"Users updated\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to update item, please see server console for more info.\"}";
+        }
+    }
+    @POST
+    @Path("delete/{UserID}") //Delete method to delete a record from the database
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String DeleteUser(@PathParam("UserID") Integer UserID) throws Exception {
+        System.out.println("Invoked Users.DeleteUser()");
+        if (UserID == null) {
+            throw new Exception("UserID is missing in the HTTP request's URL.");
+        }
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Users WHERE userId = ?");
+            ps.setInt(1, UserID);
+            ps.execute();
+            return "{\"OK\": \"User deleted\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to delete item, please see server console for more info.\"}";
         }
     }
 }
