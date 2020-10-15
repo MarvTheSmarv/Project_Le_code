a1bada90902e94ead2ad84eccaa32775d5fd27fc
92886 <92886@farnborough.ac.uk>
2020-10-15 12:09:42 +0100

    yoyo

--- resources/client/js/users.js
+++ resources/client/js/users.js
@@ -1,3 +1,4 @@
+//getUser() Returns all rows of data from the data base (First button)
 "use strict";
 function getUsersList() {
     debugger;
@@ -15,8 +16,6 @@ function getUsersList() {
     }
 });
 }
-
-
 function formatUsersList(myJSONArray){
     let dataHTML = "";
     for (let item of myJSONArray) {
@@ -24,3 +23,43 @@ function formatUsersList(myJSONArray){
     }
     document.getElementById("UsersTable").innerHTML = dataHTML;
 }
+
+//getUser() returns one row of data from the database using a GET and path parameter (Second button)
+function getUser() {
+    console.log("Invoked getUser()");     //console.log your BFF for debugging client side
+    const userId = document.getElementById("userId").value;  //get the UserId from the HTML element with id=UserID
+    //let UserID = 1; 			  //You could hard code it if you have problems
+    debugger;				  //debugger statement to allow you to step through the code in console dev F12
+    const url = "/users/get/";       // API method on webserver
+    fetch(url + userId, {                // UserID as a path parameter
+        method: "GET",
+    }).then(response => {
+        return response.json();                         //return response to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {         //checks if response from server has an "Error"
+            alert(JSON.stringify(response));            // if it does, convert JSON object to string and alert
+        } else {
+            document.getElementById("DisplayOneUser").innerHTML = response.userId + " " + response.name;  //output data
+        }
+    });
+}
+
+//addUser function to add a user to the database
+function addUser() {
+    console.log("Invoked AddUser()");
+    const formData = new FormData(document.getElementById('InputUserDetails'));
+    let url = "/users/add";
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    }).then(response => {
+        return response.json()
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));
+        } else {
+            window.open("/client/index.html", "_self");   //URL replaces the current page.  Create a new html file
+        }                                                  //in the client folder called welcome.html
+    });
+}
+
