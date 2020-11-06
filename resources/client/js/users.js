ec8d91c3f4f772cbba588f6aff4924403f4ad4fe
92886 <92886@farnborough.ac.uk>
2020-11-06 15:57:21 +0000

    yoyo

--- resources/client/js/users.js
+++ resources/client/js/users.js
@@ -63,3 +63,45 @@ function addUser() {
     });
 }
 
+function UsersLogin() {
+    //debugger;
+    console.log("Invoked UsersLogin() ");
+    let url = "/users/login";
+    let formData = new FormData(document.getElementById('LoginForm'));
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    }).then(response => {
+        return response.json();                 //now return that promise to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            Cookies.set("token", response.token);
+            Cookies.set("email", response.email);
+            window.open("index.html", "_self");       //open index.html in same tab
+        }
+    });
+}
+
+function logout() {
+    debugger;
+    console.log("Invoked logout");
+    let url = "/users/logout";
+    fetch(url, {method: "POST"
+    }).then(response => {
+        return response.json();                 //now return that promise to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            Cookies.remove("token", response.token);    //UserName and Token are removed
+            Cookies.remove("email", response.email);
+            window.open("login.html", "_self");       //open index.html in same tab
+        }
+    });
+}
+
+
+
--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -9,12 +9,69 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.UUID;
 
 @Path("users/")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
 @Produces(MediaType.APPLICATION_JSON)
 
 public class Users{
+    @POST
+    @Path("login")
+    public String UsersLogin(@FormDataParam("email") String email, @FormDataParam("password") String password) {
+        System.out.println("Invoked loginUser() on path users/login");
+        try {
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT password FROM Users WHERE email = ?");
+            ps1.setString(1, email);
+            ResultSet loginResults = ps1.executeQuery();
+            if (loginResults.next() == true) {
+                String correctPassword = loginResults.getString(1);
+                if (password.equals(correctPassword)) {
+                    String token = UUID.randomUUID().toString();
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET token = ? WHERE email = ?");
+                    ps2.setString(1, token);
+                    ps2.setString(2, email);
+                    ps2.executeUpdate();
+                    JSONObject userDetails = new JSONObject();
+                    userDetails.put("email", email);
+                    userDetails.put("token", token);
+                    return userDetails.toString();
+                } else {
+                    return "{\"Error\": \"Incorrect password!\"}";
+                }
+            } else {
+                return "{\"Error\": \"Incorrect username.\"}";
+            }
+        } catch (Exception exception) {
+            System.out.println("Database error during /users/login: " + exception.getMessage());
+            return "{\"Error\": \"Server side error!\"}";
+        }
+    }
+
+    @POST
+    @Path("logout")
+    public static String logout(@CookieParam("token") String token){
+        try{
+            System.out.println("users/logout "+ token);
+            PreparedStatement ps = Main.db.prepareStatement("SELECT userId FROM Users WHERE token = ?");
+            ps.setString(1, token);
+            ResultSet logoutResults = ps.executeQuery();
+            if (logoutResults.next()){
+                int userId = logoutResults.getInt(1);
+                //Set the token to null to indicate that the user is not logged in
+                PreparedStatement ps1 = Main.db.prepareStatement("UPDATE Users SET token = NULL WHERE userId = ?");
+                ps1.setInt(1, userId);
+                ps1.executeUpdate();
+                return "{\"status\": \"OK\"}";
+            } else {
+                return "{\"error\": \"Invalid token!\"}";
+            }
+        } catch (Exception ex) {
+            System.out.println("Database error during /users/logout: " + ex.getMessage());
+            return "{\"error\": \"Server side error!\"}";
+        }
+    }
+
     @GET
     @Path("list") //Read method to read from the database, 'curl -s localhost:8081/users/list' in git bash (ATM only displays userId and name)
     public String UsersList() {
@@ -35,6 +92,7 @@ public class Users{
             return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
         }
     }
+
     @GET
     @Path("get/{userId}") //Get one record from the database, 'curl -s localhost:8081/users/get/1' (1 is example of UserId) in git bash (Currently returns the name of the userId entered)
     public String GetUser(@PathParam("userId") Integer userId) {
@@ -57,16 +115,19 @@ public class Users{
 
     @POST
     @Path("add") //Create method to create a new record in the database, 'curl -s localhost:8081/users/add -F name='Pascal8' -F password='YesNo1' -F subMaths=True -F subCompSci=True -F subPhysics=True' for example in git bash
-    public String UsersAdd(@FormDataParam("name") String name, @FormDataParam("password") String password, @FormDataParam("subMaths") Boolean subMaths,
-                           @FormDataParam("subCompSci") Boolean subCompSci, @FormDataParam("subPhysics") Boolean subPhysics){
+    public String UsersAdd(@FormDataParam("name") String name, @FormDataParam("password") String password, @FormDataParam("email") String email,
+                           @FormDataParam("subMaths") Boolean subMaths, @FormDataParam("subCompSci") Boolean subCompSci, @FormDataParam("subPhysics") Boolean subPhysics
+                            , @FormDataParam("admin") Boolean admin){
         System.out.println("Invoked Users.UsersAdd()");
         try {
-            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (name, password, subMaths, subCompSci, subPhysics) VALUES (?, ?, ?, ?, ?)");
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (name, password, email, subMaths, subCompSci, subPhysics, admin) VALUES (?, ?, ?, ?, ?, ?, ?");
             ps.setString(1, name);
             ps.setString(2, password);
-            ps.setBoolean(3, subMaths);
-            ps.setBoolean(4, subCompSci);
-            ps.setBoolean(5, subPhysics);
+            ps.setString(3, email);
+            ps.setBoolean(4, subMaths);
+            ps.setBoolean(5, subCompSci);
+            ps.setBoolean(6, subPhysics);
+            ps.setBoolean(7, admin);
             ps.execute();
             return "{\"OK\": \"Added user. (∩─ᗝ─)⊃━☆ﾟ.*☆ﾟ *Bing*\"}";
         } catch (Exception exception) {
@@ -75,6 +136,7 @@ public class Users{
         }
 
     }
+
     @POST
     @Path("update") //Update method to update an existing record in the database, 'curl -s -X POST localhost:8081/users/update -F UserID=8 -F UserName='Toast' for example in git bash
     public String updateFood(@FormDataParam("userId") Integer userId, @FormDataParam("name") String name) {
@@ -90,6 +152,7 @@ public class Users{
             return "{\"Error\": \"Unable to update item, please see server console for more info.\"}";
         }
     }
+
     @POST
     @Path("delete/{UserID}") //Delete method to delete a record from the database, 'curl -s -X POST localhost:8081/users/delete/8' (8 is example of userId) in git bash
 
